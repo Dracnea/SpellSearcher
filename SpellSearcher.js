56029bdee138815bb7d3e8790fcd71f1c2226043
Dracnea <dracneagenie@gmail.com>
2021-03-20 20:29:51 -0400

    Add Title

---
+++ SpellSearcher.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name         Backpack.tf Spell Searcher
+// @namespace    http://tampermonkey.net/
+// @version      9.0
+// @description  A script to web crawl through backpack.tf and gather data regarding halloween spelled items.
+// @author       Dracnea
+// @match        https://backpack.tf/premium*
+// @grant        none
+// ==/UserScript==
+
